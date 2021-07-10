e856d289f0ec31aa49643d9854de170738cb4912
Minnow Brine <minnowbrine@live.ca>
2021-07-10 19:08:24 -0400

    updating the gitignore because i'm really good at using git

---
+++ HelperLibs/Types/MainFormSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinkingCat.HelperLibs
+{
+    public static class MainFormSettings
+    {
+        public static event EventHandler SettingsChangedEvent;
+        public static bool hideMainFormOnCapture { get; set; } = true;
+        public static bool showInTray { get; set; } = true;
+        public static bool minimizeToTray { get; set; } = true;
+        public static bool startInTray { get; set; } = false;
+        public static bool alwaysOnTop { get; set; } = true;
+        public static int waitHideTime { get; set; } = 300;
+
+        public static Tasks onTrayLeftClick { get; set; } = Tasks.RegionCapture;
+        public static Tasks onTrayDoubleLeftClick { get; set; } = Tasks.OpenMainForm;
+        public static Tasks onTrayMiddleClick { get; set; } = Tasks.NewClipFromClipboard;
+
+        public static void OnSettingsChangedEvent()
+        {
+            if (SettingsChangedEvent != null)
+            {
+                SettingsChangedEvent(null, EventArgs.Empty);
+            }
+        }
+    }
+}
